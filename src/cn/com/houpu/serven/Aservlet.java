4ff3e8e8ef5fcd9ee893a392517840be0fd59dfb
yang <825025679@qq.com>
2020-04-16 14:36:46 +0800

    初始化

---
+++ src/cn/com/houpu/serven/Aservlet.java
@@ -0,0 +1,28 @@
+package cn.com.houpu.serven;
+
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Aservlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	System.out.println("1");
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("2");
+	}
+
+	
+	
+	
+	
+
+}
